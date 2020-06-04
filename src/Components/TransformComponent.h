57006879a2563da8037156f9b0fb3f2cf11b2b7f
vincgonzo <vincgonzo@hotmail.fr>
2020-06-04 23:38:10 +0200

    SpriteComponent init and bug to fix

--- src/Components/TransformComponent.h
+++ src/Components/TransformComponent.h
@@ -2,11 +2,11 @@
 #define TRANSFORMCOMPONENT_H
    
 #include "../EntityManager.h"
-   #include "../../lib/glm/glm.hpp"
-   #include <SDL2/SDL.h>
-   #include "../Game.h"
+#include "../../lib/glm/glm.hpp"
+#include <SDL2/SDL.h>
+ #include "../Game.h"
    
-   class TransformComponent: public Component {
+class TransformComponent: public Component {
        public:
            glm::vec2 position;
            glm::vec2 velocity;
@@ -32,16 +32,9 @@
            }
    
            void Render() override {
-               SDL_Rect  transformRectangle = {
-                   (int) position.x,
-                   (int) position.y,
-                   width,
-                   height
-               };
-               SDL_SetRenderDrawColor(Game::renderer, 255, 255, 255, 255);
-               SDL_RenderFillRect(Game::renderer, &transformRectangle);
+
            }
-   };
+};
    
-   #endif
+#endif
 
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "./Constants.h"
 #include "./Game.h"
+#include "./AssetManager.h"
 #include "./Components/TransformComponent.h"
+#include "./Components/SpriteComponent.h"
 #include "../lib/glm/glm.hpp"
 
 EntityManager manager;
+AssetManager* Game::assetManager = new AssetManager(&manager);
 SDL_Renderer* Game::renderer;
 
 Game::Game(){
@@ -51,12 +54,15 @@ void Game::Initialize(int width, int height) {
 }
 
 void Game::LoadLevel(int levelNumber){
-    Entity& newEntity(manager.AddEntity("projectile"));
+    // Start include new assets to assetManager
+    std::string textureFilePath = "./assets/images/truck-down.png";
+    //std::string textureFilePath = "./assets/images/tank-big-right.png";
+    assetManager->AddTexture("tank-image", textureFilePath.c_str());
+
+    // Start include entities and also component to them
+    Entity& newEntity(manager.AddEntity("tank"));
     newEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
-    Entity& PP2(manager.AddEntity("projectile2"));
-    PP2.AddComponent<TransformComponent>(10, 10, 20, 20, 32, 32, 1);
-    Entity& Py3(manager.AddEntity("projectile3"));
-    Py3.AddComponent<TransformComponent>(100, 100, 10, 10, 15, 15, 1);
+    newEntity.AddComponent<SpriteComponent>("tank-image");
 }
 
 void Game::ProcessInput(){
